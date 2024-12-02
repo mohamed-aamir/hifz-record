cd4faf444a97b18b046a4aa378f0a78bf63b84e2
Mohamed Aamir <104039761+mohamed-aamir@users.noreply.github.com>
2024-12-02 07:05:28 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'tracker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/style.css',
+  '/script.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch resources from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
